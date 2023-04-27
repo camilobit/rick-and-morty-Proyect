f7bcf616e1e3d77180f82d67bba3e0e86b4c917d
camilobit <camiloacevedovargas@gmail.com>
2023-04-27 07:49:09 -0500

    homework m3-05

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,7 @@
-const http = require('http');
-const { getCharById } = require('./controllers/getCharById');
+const express = require('express');
+const server = express();
+const PORT = 3001;
 
-http
-.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    if(req.url.includes('/rickandmorty/character')){
-        const id = req.url.split('/').at(-1);
-
-        getCharById(res, +id)
-    }
-})
-.listen(3001, 'localhost')
+server.listen(PORT, () => {
+    console.log('Server raised in port: ' + PORT);
+});
